2e4c84b6b299d7c39a4fffeee417f33e250d752b
Deepesh <deepeshch2k9@gmail.com>
2017-10-26 19:56:12 +0530

    added donate option

--- app/src/main/java/in/dc297/mqttclpro/activity/BrokersListActivity.java
+++ app/src/main/java/in/dc297/mqttclpro/activity/BrokersListActivity.java
@@ -9,6 +9,8 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
@@ -54,16 +56,6 @@ public class BrokersListActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
-        FloatingActionButton donationFab = (FloatingActionButton) findViewById(R.id.fabDonate);
-        donationFab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),DonationActivity.class);
-                startActivity(intent);
-            }
-        });
-
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         data = ((MQTTClientApplication) getApplication()).getData();
         executor = Executors.newSingleThreadExecutor();
@@ -93,6 +85,28 @@ public class BrokersListActivity extends AppCompatActivity {
         executor.shutdown();
         adapter.close();
         super.onDestroy();
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_brokers_list, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if(id == R.id.action_donate){
+            Intent donateIntent = new Intent(this,DonationActivity.class);
+            startActivity(donateIntent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     public class BrokersListAdapter extends QueryRecyclerAdapter<BrokerEntity, BindingHolder<BrokerListItemBinding>> implements View.OnClickListener, View.OnLongClickListener {
